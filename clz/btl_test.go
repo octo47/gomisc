8e05d8f8db476af96df18767c16c942194271915
Andrey Stepachev <sandrey@dropbox.com>
2016-09-28 06:52:48 -0700

    removing asm, adding Harley hamming

--- clz/btl_test.go
+++ clz/btl_test.go
@@ -5,17 +5,6 @@ import (
 	"testing"
 )
 
-// assembly from math/big/arith_386.s
-// TEXT ·bitlenAsm(SB),7,$0
-//         BSRL x+0(FP), AX
-//         JZ Z1
-//         INCL AX
-//         MOVL AX, n+4(FP)
-//         RET
-//
-// Z1:     MOVL $0, n+4(FP)
-//         RET
-
 // exp[(1<<i) % 37] = i
 var exp [37]uint8
 
@@ -35,16 +24,47 @@ func bitlen(a uint32) int {
 	return int(exp[(a%37)+1])
 }
 
+const (
+	m1  = uint64(0x5555555555555555)
+	m2  = uint64(0x3333333333333333)
+	m4  = uint64(0x0F0F0F0F0F0F0F0F)
+	h01 = uint64(0x0101010101010101)
+)
+
+func bitlen64(a uint64) int {
+	a |= a >> 1
+	a |= a >> 2
+	a |= a >> 4
+	a |= a >> 8
+	a |= a >> 16
+	a |= a >> 32
+	return popcount(a)
+}
+
+// using mult based hamming code (number of '1' in a bit string)
+// https://en.wikipedia.org/wiki/Hamming_weight
+func popcount(x uint64) int {
+	x -= (x >> 1) & m1             //put count of each 2 bits into those 2 bits
+	x = (x & m2) + ((x >> 2) & m2) //put count of each 4 bits into those 4 bits
+	x = (x + (x >> 4)) & m4        //put count of each 8 bits into those 8 bits
+	return int((x * h01) >> 56)    //returns left 8 bits of x + (x<<8) + (x<<16) + (x<<24) + ...
+}
+
 func TestBitlen(t *testing.T) {
-	t.Log(bitlen(17))
+	bl1 := bitlen(17)
+	var buf big.Int
+	bl2 := bitlenBig(17, &buf)
+	bl3 := bitlen64(17)
+	if bl1 != bl2 || bl2 != bl3 {
+		t.FailNow()
+	}
+	t.Log(bl1, bl2, bl3)
 }
 
 func bitlenBig(a uint32, buf *big.Int) int {
 	buf.SetInt64(int64(a))
 	return buf.BitLen()
 }
-
-func bitlenAsm(a uint32) int
 
 var n int
 
@@ -66,7 +86,7 @@ func benchmark2(b *testing.B, v uint32) {
 func benchmark3(b *testing.B, v uint32) {
 	x := v
 	for i := 0; i < b.N; i++ {
-		n = bitlenAsm(x)
+		n = bitlen64(uint64(x))
 	}
 }
 
